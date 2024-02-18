dd209d9f3fa1f287e8eae0a3afa607c0d472eb0a
ecomnazar <ecomnazar.me@gmail.com>
2024-02-18 17:38:46 +0500

    [added]: register

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,34 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Controller } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as argon2 from 'argon2';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findOne(email);
+    const passwordIsMatch = await argon2.verify(user.password, password);
+    if (user && passwordIsMatch) {
+      return user;
+    }
+    throw new BadRequestException('User or password an incorrect');
   }
 }
