33db12330953476f03c281a99f35e8d368eb866d
ecomnazar <ecomnazar.me@gmail.com>
2024-02-19 09:01:49 +0500

    [added]: token to register

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,15 +1,16 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
   async create(createUserDto: CreateUserDto) {
     const existsUser = await this.userRepository.findOne({
@@ -24,7 +25,9 @@ export class UserService {
       password: await argon2.hash(createUserDto.password),
     });
 
-    return { user };
+    const token = this.jwtService.sign({ email: createUserDto.email });
+
+    return { user, token };
   }
 
   async findOne(email: string) {
